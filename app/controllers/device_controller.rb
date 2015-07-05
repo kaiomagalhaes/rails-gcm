0256018d9e6a24125b4e8d8f385ccec8109e71a7
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2015-07-05 17:33:34 -0300

    corrigindo parametros

--- app/controllers/device_controller.rb
+++ app/controllers/device_controller.rb
@@ -1,8 +1,10 @@
 class DeviceController < ApplicationController
   skip_before_filter :verify_authenticity_token
   def register
-    if Device.where(reg_id: params[:reg_id]).count == 0
-      device = Device.new(:reg_id => params[:reg_id], :user_id => params[:user_id], :os => 'android')
+    device_id = params[:reg_id]
+    puts "Id: #{device_id}"
+    if Device.where(reg_id: device_id).count == 0
+      device = Device.new(:reg_id => device_id, :user_id => params[:user_id], :os => 'android')
       device.save!
     end
     puts Device.all.count
